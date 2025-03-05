0415ed64cae7852a7e09df45ea07c5f637eeb2e5
Edgar <muchemiedgar2@gmail.com>
2025-03-05 15:58:10 +0300

    Complete JavaScript scope lab implementation

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+// 1. Declare customerName in global scope
+var customerName = 'bob';
+
+// 2. Function to uppercase customerName
+function upperCaseCustomerName() {
+    // Modify the global customerName variable directly
+    customerName = customerName.toUpperCase();
+    // Return the modified value
+    return customerName;
+}
+
+// 3. Function to set bestCustomer in global scope
+function setBestCustomer() {
+    // Set bestCustomer to 'not bob' as originally specified
+    window.bestCustomer = 'not bob';
+}
+
+// 4. Function to overwrite bestCustomer
+function overwriteBestCustomer() {
+    // Overwrite bestCustomer with 'maybe bob'
+    window.bestCustomer = 'maybe bob';
+}
+
+// 5. Declare leastFavoriteCustomer as a constant
+const leastFavoriteCustomer = 'definitely not bob';
+
+// 6. Function attempting to change leastFavoriteCustomer (will fail)
+function changeLeastFavoriteCustomer() {
+    // This will throw an error because leastFavoriteCustomer is a constant
+    leastFavoriteCustomer = 'bob';
+}
